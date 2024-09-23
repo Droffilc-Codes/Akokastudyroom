bdcc84555ff6aba63e28ac382facbce6b989d1d7
Droffilc-codes <cliffordchisom@gmail.com>
2024-09-23 09:14:56 +0100

    Header realignment for Mobile Responsiveness

--- src/Header.js
+++ src/Header.js
@@ -1,39 +1,80 @@
 import React from 'react'
-import './Header.css'
+import classes from './Header.module.css'
 import logo from './images/logo.png'
 import HeaderOptions from './HeaderOptions'
 import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div className='header'>
-        <div className="header__left">
+        <header className={classes.header}>
+            <div className={classes.container}>
+
+                <Link to="/">
+                    <img src={logo} alt="" />
+                </Link>
+
+                <nav>
+                    <ul>
+
+                        <li>
+                            <Link to="/regulations">
+                                <HeaderOptions title="Regulations" />
+                            </Link> 
+                        </li>
+
+                        <li>
+                            <Link to="/Payment">
+                                <HeaderOptions title="Payment" />
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/Others">
+                                <HeaderOptions title="Others" />
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/Assessment">
+                                <HeaderOptions title="Onboarding Assesment" />
+                                {/* <HeaderOptions title="Onboarding" /> */}
+                            </Link>
+                        </li>
+
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    
+    // <div className='header'>
+
+    //     <div className="header__left">
             
-            <Link to="/">
-                <img src={logo} alt="" />
-            </Link>
+    //         <Link to="/">
+    //             <img src={logo} alt="" />
+    //         </Link>
                 
         
-        </div>
+    //     </div>
 
-        <div className="header__right">
+    //     <div className="header__right">
            
+    //         <Link to="/regulations">
+    //             <HeaderOptions title="Regulations" />
+    //         </Link>   
+            // <Link to="/Payment">
+            //     <HeaderOptions title="Payment" />
+            // </Link>
+    //         <Link to="/Others">
+    //             <HeaderOptions title="Others" />
+    //         </Link>
+    //         <Link to="/Assessment">
+    //             <HeaderOptions title="Onboarding Assesment" />
+    //         </Link>
 
-            <Link to="/regulations">
-                <HeaderOptions title="Regulations" />
-            </Link>   
-            <Link to="/Payment">
-                <HeaderOptions title="Payment" />
-            </Link>
-            <Link to="/Others">
-                <HeaderOptions title="Others" />
-            </Link>
-            <Link to="/Assessment">
-                <HeaderOptions title="Onboarding Assesment" />
-            </Link>
+    //     </div>
 
-        </div>
-    </div>
+    // </div>
   )
 }
 
