fab752a868256bd7e1c62ac5244ef23caa07bc9d
Droffilc-codes <cliffordchisom@gmail.com>
2024-10-06 09:48:27 +0100

    Others page v1.0

--- src/pages/Others.js
+++ src/pages/Others.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react'
 import './Others.css'
 import toilet from '../images/toilete.webp'
+import seat from '../images/seat.webp'
+import brush from '../images/brush.webp'
 
 
 
@@ -8,11 +10,11 @@ import toilet from '../images/toilete.webp'
 function Others() {
   return (
     <Fragment>
-    <div className='clothes'>
+    <div className='Others_heading'>
       <h3>Others to select from</h3>
   
 
-      <div className="clothes__container">
+      <div className="Others__container">
           {/* <p>Spirit of Servince</p>
 
           <p>How to take care of the Toilet</p>
@@ -21,15 +23,34 @@ function Others() {
           <p>List of material Assignment</p>
           <p>Announcement and News</p>
           <p>Straight A club</p> */}
-        
-        <div>
-            <h3>How to take care of the Toilet</h3>
-            <div className='Toilet'>
-              <img src={toilet} className='toilet_image' alt='restroom'/>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus unde voluptatem, optio soluta quae tenetur adipisci. Nostrum nam inventore recusandae? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae doloribus quod vitae quia enim odio consectetur deleniti non temporibus necessitatibus. Ratione nesciunt dolore quod facilis ut, dolores iusto consectetur neque magnam quisquam ipsum harum at, ducimus aut numquam eaque cumque voluptatem sint! Doloremque nam excepturi expedita nobis nisi aperiam labore?</p>
-            </div>
-        </div>
 
+              <h3 className='Restroom_Text'>Using the Restroom </h3>
+      
+            <div className='main'>
+              <div className='pics'>
+                <img src={toilet} alt='somepics' className='toilet_image'/>
+                <img src={seat} className='toilet_image' alt='someimage' />
+                <img src={brush} className='toilet_image' alt='someimage' />
+              </div>
+              <div>
+                <p>The Toilet by the Study room is meant for visitors and members of the Study room. A rule thumb is that the toilet is clean only when you can eat comfortably in it. For that reason, it is imperative to ensure that it is clean. If you come to make use of the toilet, the objective is to leave it better than you met it. What does this entail? It means that you will have to ensure that the bowl is clean, the seat is clean, and the floor is clean. A piece of tissue paper can be used to clean the seats and the bowl or in some cases the brush can be applied.</p>
+                <p>We have the bucket underneath the handwash basin. You can get water from the tap underneath the basing to flush hard </p>
+                <ul>
+                  <li>Always close the door behind you when entering the restroom to maintain privacy and respect for others.</li>
+                  <li>Stand close to the toilet seat to avoid dripping</li>
+                  <li>Always flush the toilet after use.</li>
+                  <li>Clean floor with mob</li>
+                  <li>Clean toilet seat and bowl with tissue paper</li>
+                  <li>Ensure to leave the bowl bright and clean better than you met it.</li>
+                </ul>
+              </div>
+            </div>
+
+
+            <div>
+              <h2>Power Usage and Consumption</h2>
+              <p>We try to provide power to the house from two and in some cases three sources. There is an Inverter for backup and the power from the grid. Power has become very expensive, hence it is everybody's responsibility to manage consumpton. When you become a member of the study room, you have also become a caretaker and responsible for the running of the house. It is everyone's duty to remind others about the need to take care of consumption. Unplugging the laptop when already full is a good action. It is also important to know that bring powerbanks to charge with the inverter when there is a national blackout is not fair. It consumes the backup battery and makes it difficult for the residents of Akoka to make use of electrci power when they get back home from their various work places. Be conscious of this.</p>
+            </div>
       </div>
       
     </div>
