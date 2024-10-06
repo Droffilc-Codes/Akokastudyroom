a3ca1aa73bf777c73a8673e27c1c8e5129074367
Droffilc-codes <cliffordchisom@gmail.com>
2024-10-06 21:31:57 +0100

    Onboarding Completed

--- src/pages/Assessment.js
+++ src/pages/Assessment.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import classes from './Assessment.module.css'
+import { Link } from 'react-router-dom'
 
 export default function Assessment() {
   return (
     <div className={classes.container}>
+      <h1>
       Please take your initial Assessment
-      <p>This is part of your onboarding</p>
+      </h1>
+      <h3>Without this Assessment you will not be called for the interview and your desire to be part of Akoka Study Centre will be discontinued</h3>
+
+      <Link to="https://forms.gle/Z1WBhKzUjNBMxH9u9" className={classes.assessmentLink}>
+        <p>Click here to take your initial Assessment</p>
+      </Link>
+
+
     </div>
   )
 }
